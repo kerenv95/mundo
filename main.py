8ea9fc790e26b015702f62d8df48c2e451f2ad16
kerenv95 <105228924+kerenv95@users.noreply.github.com>
2022-05-12 08:46:10 -0500

    ok

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ pais = input()
 
 if pais in continentes["America"]:
     print(continentes["America"][pais]["capital"])
-    print(continentes["America"][pais]["language"])
-    print(continentes["America"][pais]["cities"])
+    print(continentes["America"][pais]["idiomas"])
+    print(continentes["America"][pais]["ciudades"])
 
 
 if pais in continentes["Africa"]:
     print(continentes["Africa"][pais]["capital"])
-    print(continentes["America"][pais]["language"])
-    print(continentes["America"][pais]["cities"])
+    print(continentes["America"][pais]["idiomas"])
+    print(continentes["America"][pais]["ciudades"])
