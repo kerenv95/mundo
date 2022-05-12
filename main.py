0477efe0220ddb87d79d7c0960c6c648fb450ae3
kerenv95 <105228924+kerenv95@users.noreply.github.com>
2022-05-12 08:48:59 -0500

    ok

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ if pais in continentes["America"]:
     print(continentes["America"][pais]["capital"])
     print(continentes["America"][pais]["idiomas"])
     print(continentes["America"][pais]["ciudades"])
+    print(continentes["America"][pais]["gentilicio"])
 
 
 if pais in continentes["Africa"]:
     print(continentes["Africa"][pais]["capital"])
-    print(continentes["America"][pais]["idiomas"])
-    print(continentes["America"][pais]["ciudades"])
+    print(continentes["Africa"][pais]["idiomas"])
+    print(continentes["Africa"][pais]["ciudades"])
+    print(continentes["Africa"][pais]["gentilicio"])
